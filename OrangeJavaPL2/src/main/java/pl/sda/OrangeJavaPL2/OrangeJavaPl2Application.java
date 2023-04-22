b861c7eb29b5d917d0baa63768175f40728fef1d
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-04-22 03:52:55 +0200

    Init second SpringBoot app

---
+++ OrangeJavaPL2/src/main/java/pl/sda/OrangeJavaPL2/OrangeJavaPl2Application.java
@@ -0,0 +1,13 @@
+package pl.sda.OrangeJavaPL2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeJavaPl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeJavaPl2Application.class, args);
+	}
+
+}
---
+++ OrangeJavaPL2/src/test/java/pl/sda/OrangeJavaPL2/OrangeJavaPl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.OrangeJavaPL2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeJavaPl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
